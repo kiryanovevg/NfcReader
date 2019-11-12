20fb1db0c0b807fe704809817ad3cb9f3b1ca46e
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.team>
2019-11-12 19:39:32 +0300

    tests

--- app/src/main/java/com/roonyx/nfcreader/MainActivity.kt
+++ app/src/main/java/com/roonyx/nfcreader/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.roonyx.nfcreader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val nfcHelper = NfcHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var count = 0
+
+        nfcHelper.apply {
+            initialize(this@MainActivity)
+            uidLiveData.observe(this@MainActivity, Observer {
+                val str = "count: ${++count}\nrfid: $it"
+                textView.text = str
+            })
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.also { nfcHelper.handleIntent(intent) }
     }
 }
---
+++ app/src/main/java/com/roonyx/nfcreader/NfcHelper.kt
@@ -0,0 +1,85 @@
+package com.roonyx.nfcreader
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.*
+
+class NfcHelper {
+
+    private val uid = MutableLiveData<String>()
+    val uidLiveData: LiveData<String> get() = uid
+
+    fun initialize(fragmentActivity: FragmentActivity) {
+        val nfcAdapter = NfcAdapter.getDefaultAdapter(fragmentActivity)
+        val techLists = arrayOf(arrayOf(Ndef::class.java.name))
+        val pendingIntent = PendingIntent.getActivity(
+            fragmentActivity, REQUEST_CODE_NFC,
+            Intent(fragmentActivity, fragmentActivity.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            0
+        )
+
+        val intentFilter = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+
+        try {
+            intentFilter.addDataType(MIME_TYPE_NFC)
+        } catch (e: IntentFilter.MalformedMimeTypeException) {
+            throw RuntimeException("Error while adding data type", e)
+        }
+
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT)
+        val filters = arrayOf(intentFilter)
+
+        fragmentActivity.lifecycle.addObserver(object : LifecycleObserver {
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume() {
+                nfcAdapter.enableForegroundDispatch(
+                    fragmentActivity, pendingIntent, filters, techLists
+                )
+            }
+
+            /*@OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause() {
+                nfcAdapter.disableForegroundDispatch(fragmentActivity)
+            }*/
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                fragmentActivity.lifecycle.removeObserver(this)
+            }
+        })
+    }
+
+    fun handleIntent(intent: Intent) {
+        if (intent.action == NfcAdapter.ACTION_TAG_DISCOVERED
+            && intent.type == MIME_TYPE_NFC) {
+
+            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+            uid.value = byteArrayToHexString(tag.id)
+        }
+    }
+
+    private fun byteArrayToHexString(array: ByteArray) = StringBuilder().apply {
+        val hex = arrayOf("0", "1", "2", "3", "4", "5", "6",
+            "7", "8", "9", "A", "B", "C", "D", "E", "F")
+
+        repeat(array.size) { i ->
+            if (isNotEmpty()) append(":")
+            val input = array[i].toInt() and 0xff
+            var j = input shr 4 and 0x0f
+            append(hex[j])
+            j = input and 0x0f
+            append(hex[j])
+        }
+    }.toString()
+
+    companion object {
+        const val REQUEST_CODE_NFC = 111
+        const val MIME_TYPE_NFC = "application/com.roonyx.nfcreader.nfc"
+    }
+}
